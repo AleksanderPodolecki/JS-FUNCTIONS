b05d4e27c4718cb990b11d958a2c8be9c0f7f759
AleksanderPodolecki <73857662+AleksanderPodolecki@users.noreply.github.com>
2022-06-12 14:52:59 +0200

    Add files via upload

---
+++ odwracanie_znaków_w_string.c
@@ -0,0 +1,14 @@
+    #include <stdio.h>
+    #include <string.h>
+
+
+    int main()
+    {
+        char str[40] = "olek123";
+        printf ("Przd odwroceniem: %s\n", str );
+
+
+
+        printf (" \nPo odwroceniu: %s\n ", strrev(str));
+        return 0;
+    }
