533e16aafc806a703ad09656ff1b17a4daead392
AleksanderPodolecki <73857662+AleksanderPodolecki@users.noreply.github.com>
2022-06-12 21:25:28 +0200

    Update odwracanie_string_2.c

--- odwracanie_string_2.c
+++ odwracanie_string_2.c
@@ -1,17 +1,26 @@
 #include<stdio.h>
-//#include<string.h>
+
         
 void main()
 {
-    int i, n;
+    int i;
     char str[10] = "aleksander";
+    char *ptr = str;
     
-    //n=strlen(str);
+    //printf("str[]: %c\n", str[9]);
+    //printf("*ptr:  %c\n", *(ptr+9));
     
     printf("\nOdwrocone slowo to: ");
     for(i=10;i>=0;i--) //i = wielkosc str
     {
        printf("%c",str[i]);
     }
+    
+    printf("\nOdwrocone slowo za pomocą wskaźnika to: ");
+    for(i = 10; i >= 0; i--)
+    {
+        printf("%c", *(ptr + i));    
+    }
+    
 
 }
